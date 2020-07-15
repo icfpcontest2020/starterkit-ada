659b28e882312471e0b7f7ab71e9b5e31b0dcffb
John Warwick <jwarwick@gmail.com>
2020-07-15 15:03:06 -0400

    Checking status code directly. Added http code reason on error.

--- app/src/main.adb
+++ app/src/main.adb
@@ -4,6 +4,7 @@ with AWS.Client;
 with AWS.Headers;
 with AWS.Response;
 with AWS.Messages;
+use AWS.Messages;
 
 procedure main is
   hdrs : AWS.Headers.List := AWS.Headers.Empty_List;
@@ -17,11 +18,11 @@ begin
   AWS.Headers.Add(hdrs, "Content-Type", "text/plain");
   result := AWS.Client.Post(URL => server_url, Data => player_key, Headers => hdrs);
   status := AWS.Response.Status_Code(result);
-  if AWS.Messages.Image(status) = "200" then
+  if status = AWS.Messages.S200 then
     Text_IO.Put_Line("Server response: " & AWS.Response.Message_Body(result));
   else
     Text_IO.Put_Line("Unexpected server response:");
-    Text_IO.Put_Line("HTTP code: " & AWS.Messages.Image(status));
+    Text_IO.Put_Line("HTTP code: " & AWS.Messages.Image(status) & " (" & AWS.Messages.Reason_Phrase(status) & ")");
     Text_IO.Put_Line("Response body: " & AWS.Response.Message_Body(result));
     Ada.Command_Line.Set_Exit_Status(2);
   end if;
